79626b58dbb52c620e2beff42c74a0d79038313f
justinelite <justin858@hotmail.com>
2016-07-02 12:48:26 +1000

    first commit make it better

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name apsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the apsApp
+ */
+angular.module('apsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
