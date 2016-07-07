b92f874b3eefc9d4a054f131c7f34fd9973aba72
justinelite <justin858@hotmail.com>
2016-07-07 21:10:15 +1000

    gruntfile update for Heroku deployment

--- Gruntfile.js
+++ Gruntfile.js
@@ -462,16 +462,18 @@ module.exports = function (grunt) {
 
   grunt.registerTask('heroku', 'Compile then start a connect web server', function (target) {
     if (target === 'dist') {
-      return grunt.task.run(['build', 'connect:dist:keepalive']);
+      return grunt.task.run(['build'
+      //, 'connect:dist:keepalive'
+      ]);
     }
 
     grunt.task.run([
       'clean:server',
       'wiredep',
       'concurrent:server',
-      'postcss:server',
-      'connect:livereload',
-      'watch'
+      'postcss:server'//,
+      //'connect:livereload',
+      //'watch'
     ]);
   });
 
